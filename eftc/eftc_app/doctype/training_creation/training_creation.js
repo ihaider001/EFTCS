ef4f25c733c57bcdce3be4225d74302242045791
Honey <honey.sharma@nestorbird.com>
2023-08-19 11:37:12 +0530

    Quotation Follow Up Notification

---
+++ eftc/eftc_app/doctype/training_creation/training_creation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, NestorBird and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Training Creation', {
+	// refresh: function(frm) {
+
+	// }
+});
