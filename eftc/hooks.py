d3a6fedbfcbbf203906f73077b9a002e3c03436b
@anil <anil@nestorbird.com>
2023-11-02 17:07:24 +0530

    implementation on sales order

--- eftc/hooks.py
+++ eftc/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/eftc/css/eftc.css"
+app_include_css = "/assets/eftc/css/print.css"
 # app_include_js = "/assets/eftc/js/eftc.js"
 
 # include js, css files in header of web template
