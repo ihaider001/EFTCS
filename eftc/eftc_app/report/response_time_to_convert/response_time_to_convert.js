6c672e9ddbd0a96bbd93ff8b020f3f2c6c4fd5f3
rajatnesto <rajat.tripathi@nestorbird.com>
2023-11-26 06:16:41 +0000

    Response Time To Convert

---
+++ eftc/eftc_app/report/response_time_to_convert/response_time_to_convert.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, NestorBird and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Response Time To Convert"] = {
+	"filters": [
+
+	]
+};
